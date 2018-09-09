3833b9ad7b0adf1afef49c73db01e0cddd3d1513
willgower <35236515+willgower@users.noreply.github.com>
2018-09-09 15:01:28 +0100

    Update pixartlite.py

--- pixartlite.py
+++ pixartlite.py
@@ -269,7 +269,7 @@ char_array = {"A" : [False, False, True, False, False, False, True, False, True,
 "£" : [False, False, True, True, False, False, True, False, False, False, False, True, False, False, False, True, True, True, True, False, False, True, False, False, False, False, True, False, False, True, True, False, True, True, False],
 " " : [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False],
 "/" : [False, False, False, False, True, False, False, False, True, False, False, False, False, True, False, False, False, True, False, False, False, True, False, False, False, False, True, False, False, False, True, False, False, False, False],
-"°" : [False, True, True, True, False, False, True, False, True, False, False, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
+"\"" : [False, True, True, True, False, False, True, False, True, False, False, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
 }
 
 matrix = []
